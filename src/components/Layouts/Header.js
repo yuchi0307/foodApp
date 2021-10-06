b463b627681f5d61bf444169bee8cdf52ccefbdc
yuchi0307 <gelee0307@gmail.com>
2021-10-06 21:46:54 +0800

    add css module

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css"
 const Header = (props) => {
   return (
     <Fragment>
-      <Header>
+      <Header className={classes.header}>
         <h1>LovingMeals</h1>
         <button>cart</button>
       </Header>
-      <div>
+      <div className={classes['main-image']}>
         <img src={mealsImage} alt="great melas waiting for you!!!"/>
       </div>
     </Fragment>
