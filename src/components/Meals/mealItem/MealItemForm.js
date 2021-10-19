e6177b16e43f5065057d463825c8966f090659a4
yuchi0307 <gelee0307@gmail.com>
2021-10-19 21:38:05 +0800

    把使用者輸入的數量反應到購物車（useRef）

--- src/components/Meals/mealItem/MealItemForm.js
+++ src/components/Meals/mealItem/MealItemForm.js
@@ -1,10 +1,30 @@
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
+import { useRef, useState } from 'react';
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef()
+  const [isAmountValid, setisAmountValid] = useState(true);
+  const submitHandler = event =>{
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if(enteredAmount.trim().length === "" ||
+    enteredAmountNumber < 1 || 
+    enteredAmountNumber > 5)
+    {
+      setisAmountValid(false);
+    }
+    props.onAddToCart(enteredAmountNumber); 
+    //將客戶點選的餐點資訊傳到購物車 
+    //先傳到mealItem 隨著表單的數量還需要meaiItem的id price等資料一起丟入購物車
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef} 
+        //注意這是客製化的component，無法直接使用ref，須到該組建中使用React.forwardRef
         label="Amount"
         input={{
           id: "amount_"+props.id,
@@ -16,6 +36,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button className={["form button"]}>+ Add</button>
+      {!isAmountValid && <p>Please entered a valid amount(1-5).</p>}
     </form>
   );
 };
--- src/components/Meals/mealItem/mealItem.js
+++ src/components/Meals/mealItem/mealItem.js
@@ -1,6 +1,22 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+
+
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const addToCartHandler = amount =>{
+      cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+    //在CartContext中，有addItem的 function，
+    //要接收參數item，這裡把id, name. amount. price包成物件做參數丟進ㄑ
+  }
   return (
     <li className={classes.meal}>
       <div>
@@ -8,7 +24,7 @@ const MealItem = (props) => {
       <div className={classes.description}>{props.description}</div>
       <div className={classes.price}>{props.price}</div>
       </div> 
-      <div><MealItemForm /></div>
+      <div><MealItemForm onAddToCart={addToCartHandler}/></div>
    </li>
   );
 };
