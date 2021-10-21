09d2116d5b227db4a1d2adc3e990c35328f787e3
yuchi0307 <gelee0307@gmail.com>
2021-10-21 22:08:31 +0800

    update CartProvider

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import classes from "./Cart.module.css";
-import Modal from '../UI/Modal';
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
+import CartItem from "./CartItem";
+
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totoalAmount = `NT$${cartCtx.totalAmount}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = id =>{};
+  const cartItemAddHandler = item =>{};
   const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[{ id: "c1", name: "suhsi", amount: "2", price: "100" }].map((item) => (
-        <li>{item.name}</li>
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -13,12 +30,14 @@ const Cart = (props) => {
     <Modal onClick={props.onClick}>
       {cartItems}
       <div className={classes.total}>
-          <span>Total Amount</span>
-          <span>100</span>
+        <span>Total Amount</span>
+        <span>{totoalAmount}</span>
       </div>
       <div className={classes.actions}>
-          <button className={classes['button--alt']} onClick={props.onClick}>Close</button>
-          <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClick}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -6,14 +6,33 @@ const defaultState = {
   totalAmount: 0,
 };
 const cartReducer = (state, action) => {
-  if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedAmount = state.totalAmount + action.item.amount * action.item.price;
-    return({
+    if (action.type === 'ADD') {
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+  
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+      const existingCartItem = state.items[existingCartItemIndex];
+      let updatedItems;
+  
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.item.amount,
+        };
+        updatedItems = [...state.items]; //將原有、未整理數量的陣列攤開
+        updatedItems[existingCartItemIndex] = updatedItem; 
+        //將整理好數量的 updatedItem 複寫回原本的位置，成為新的 updatedItems
+      } else {
+        updatedItems = state.items.concat(action.item);
+      }
+  
+      return {
         items: updatedItems,
-        totalAmount: updatedAmount
-    });
-  }
+        totalAmount: updatedTotalAmount,
+      };
+    }
   return defaultState;
 };
 const CartProvider = (props) => {
