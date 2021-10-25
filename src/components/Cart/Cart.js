f56d7da85fbef4baeff459c6f708a702f9545202
yuchi0307 <gelee0307@gmail.com>
2021-10-25 21:35:01 +0800

    made cart item removable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,16 +9,20 @@ const Cart = (props) => {
   const totoalAmount = `NT$${cartCtx.totalAmount}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id =>{};
-  const cartItemAddHandler = item =>{};
+  const cartItemRemoveHandler = id =>{
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = item =>{
+    cartCtx.addItem({...item, amount: 1})
+  };
   const cartItems = (
-    <ul className={classes["cart-items"]}>
+    <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
           name={item.name}
-          price={item.price}
           amount={item.amount}
+          price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
           onAdd={cartItemAddHandler.bind(null, item)}
         />
--- src/components/Layouts/HeaderCartButton.js
+++ src/components/Layouts/HeaderCartButton.js
@@ -1,19 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon.js";
 import cartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(cartContext);
+  const { items } = cartCtx;
+
   const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>My Cart</span>
-  <span className={classes.badge}>{numberOfCartItem}</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
   );
 };
